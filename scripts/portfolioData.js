846e2f08e335a3b7f53e28906de04a33e82f2f8d
Chris Perez <chris.k.perez@gmail.com>
2016-04-11 18:20:58 -0700

    created one project object with title, tagline, category, imagePath, projectCompleted, projectURL properties

---
+++ scripts/portfolioData.js
@@ -0,0 +1,10 @@
+var portfolioProjects = [
+  {
+    title: 'Hacker Hangout',
+    tagline: 'A visual and interactive rating site for helping developers and remote workers find a place to work',
+    projectURL: 'http://ckperez.github.io/hackerhangout',
+    category: 'design',
+    imgPath: 'img/hackerhangout.png',
+    projectCompleted: '2016-04-07'
+  }
+];
